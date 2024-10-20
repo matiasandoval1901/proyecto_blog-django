67f9991dc19f9e149863efc0505ace883a5d92fb
matiasandoval1901 <sandovalmati912@gmail.com>
2024-10-20 12:10:45 -0300

    update_config-manage.py

--- blog/manage.py
+++ blog/manage.py
@@ -2,11 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
+
+    load_dotenv()
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+
+    port = os.getenv('DJANGO_PORT')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
