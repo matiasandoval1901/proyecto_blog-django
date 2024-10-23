c64bb4282d165352080007e6aae39ede9f2b3cea
matiasandoval1901 <sandovalmati912@gmail.com>
2024-10-23 14:57:35 -0300

    update-urls_views-for_error

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView, ListView
 from apps.post.models import Post
+from django.shortcuts import render 
 
 class IndexView(ListView):
     model = Post
@@ -14,5 +15,12 @@ class IndexView(ListView):
 class AboutView(TemplateView):
     template_name = 'about.html'
 
+def not_found_view(request, exception): 
+    return render(request, 'errors/error_not_found.html', status=404)
 
+def internal_error_view(request): 
+    return render(request, 'errors/error_internal.html', status=500)
+
+def forbidden_view(request, exception): 
+    return render(request, 'errors/error_forbidden.html', status=403)
 
